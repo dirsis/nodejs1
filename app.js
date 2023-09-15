9aefe619a06253ea5162a101c73b2881c0e42460
dirsis <dirsis@gmail.com>
2023-09-15 13:14:01 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("prueba0");
+const http = require("http");
+const server = http.createServer((req,res)=> {
+    res.writeHead(200,{"Content-Type": "text/plain"});
+    res.end("Hola,Mundo");
+});
+console.log("prueba1");
+server.listen(3000,"localhost",()=> {
+    console.log("Se levanto el server");
+    
+});
+console.log("prueba2");
+setTimeout(() => {
+    server.close();
+}, 5000);
+
